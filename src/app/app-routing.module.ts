7fe769e2bb05c630f6cb095c1234fa228b55c56c
David Seek <davidseek1986@gmail.com>
2020-04-02 20:25:56 -0700

    added proper links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
       inspector: true,
     }
   }
-];
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -22,10 +22,40 @@ export class HomeComponent implements OnInit {
     this.currentMenuIndex = index 
   }
 
+  public didClickLink(index: number): void {
+    
+    if (index === 0) {
+      this.open('https://play.google.com/store/apps/details?id=de.lilithwittmann.voicepitchanalyzer&hl=en_US')
+    }
+
+    if (index === 1) {
+      this.open('https://apps.apple.com/us/app/voice-pitch-analyzer/id1302785788')
+    }
+
+    if (index === 2) {
+      this.open('https://github.com/carolanitz/VoicePitchAnalyzer')
+    }
+  }
+
   public getMenuStyle(index: number): {} {
+
     return {
       'font-weight': index === this.currentMenuIndex ? '600' : '200'
     }
   }
 
+  public getLinkClass(index: number): {} {
+
+    return {
+      'android': index === 0,
+      'apple': index === 1,
+      'github': index === 2
+    }
+  }
+
+  // MARK: - Private 
+
+  private open(url: string): void {
+    window.open(url, "_blank")
+  }
 }
